fdcc7d0bb22d29351f07f90becab3907d5a1bc84
tbushman <tbushman@pu.bli.sh>
2016-10-08 16:41:48 -0600

    pagination working. TODO: history lookup

--- server.js
+++ server.js
@@ -49,12 +49,10 @@ MongoClient.connect(uri, function (err, database) {
 		});
 	}
 });
-
-var newUrl;
 app.get('/', function(req, res){
-	newUrl = req.protocol + '://'+ req.get('host') + req.originalUrl;
 	res.render('index', {
 		title: 'FCC Image Abstraction Layer'//,
+		//page: 1
 	});
 });
 
@@ -63,19 +61,94 @@ app.get('/', function(req, res){
 	console.log(imgSrc)
 });*/
 
+//get offset
+app.get(/(.+)/, function(req, res){
+	var search_term = url.parse(req.url).pathname;
+	search_term = search_term.replace('/', '');
+	var offset = url.parse(req.url).search;
+	if (offset == undefined) {
+		offset = 0;
+	} else {		
+		offset = offset.split('=')[1];
+	}
+	var newUrl = req.protocol + '://'+ req.get('host') + '/' + search_term + '?offset='+offset;
+	var number = 10;
+	//var arr = [1, 11, 21, 31]
+	var start;
+	if (offset == 0) {
+		start = 1;
+	} else {
+		start = offset+'1'; //i.e. if offset is 1, start w/ result 11
+	}
+	var imgSrc;
+	googleSearch.build({
+		q: search_term,
+		start: start,
+		num: number
+	}, function(err, response){
+		//console.log(JSON.stringify(response.items.pagemap))
+		var list = [];
+		for (var i in response.items) {
+			//console.log(JSON.stringify(response.items[i].pagemap.cse_image[0].src));
+			var title = response.items[i].title;
+			var thumb = response.items[i].pagemap.cse_thumbnail;
+			imgSrc = response.items[i].pagemap.cse_image[0].src;
+			var entry = {title: title, thumbnail: thumb, src: imgSrc}
+			list.push(entry)
+		}
+		var now = new Date().toJSON();
+		var insertDb = {
+			search_phrase: search_term,
+			date: now
+		};
+
+		var collection = db.collection('fcc_img_search');
+		collection.insertOne(insertDb, function(err, doc){
+			if (err) {
+				handleError(res, err.message, "Failed to update DB.");
+			}
+		});
+		res.render('index', {
+			title: 'FCC Image Abstraction Layer',
+		    //display: imgSrc
+			//page: offset,
+			list: JSON.stringify(list)
+		});
+	})
+});
+/*var offset = $('#thisPage').val(); //page
+
+app.post('/offset='+offset+'', urlencodedParser, function(req, res){
+	var start = req.body.page
+});
+*/
+/*
 app.post('/', urlencodedParser, function(req, res){ //if path empty, post from form
 	var search_term = req.body.q;
+	var newUrl = 'localhost:8080/' + search_term;
+	res.redirect(newUrl)
 	//console.log(search_term)
 	var imgSrc;
 	googleSearch.build({
 		q: search_term,
-		num: 1
+		start: start,
+		num: number
 	}, function(err, response){
 		console.log(JSON.stringify(response.items[0].pagemap.cse_thumbnail[0].src));
-		imgSrc = response.items[0].pagemap.cse_thumbnail[0].src;
+		var list = [];
+		response.items.forEach(function(item){
+			imgSrc = response.items[0].pagemap.cse_thumbnail[0].src;
+			var entry = {thumbnail: imgSrc}
+			list.push(entry)
+		});
+		//imgSrc = response.items[0].pagemap.cse_thumbnail[0].src;
+		
 		res.render('index', {
 			title: 'FCC Image Abstraction Layer',
-		    display: imgSrc
+		    //display: imgSrc
+			page: offset,
+			list: JSON.stringify(list)
 		});
 	})
-});
+	
+});*/
